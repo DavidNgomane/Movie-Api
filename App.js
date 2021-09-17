10ec5df560ea8e4fc48302bad8e214fb426f0d1c
Dngomane <ngomane.david@gmail.com>
2021-09-17 15:01:59 +0200

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//import './App.css';
+import React, {useEffect, useState} from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function App() {
+    const [movie, setMovie] = useState({});
+    const [search, setSearch] = useState('');
+    
+    const API_KEY = "48066425";
+    const url = `http://www.omdbapi.com/?t=${search}&apikey=${API_KEY}`;
+
+    const getMovie = async()=>{
+      try {
+        const response = await fetch(url);
+         const data = await response.json()
+         setMovie(data);
+      } catch (error) {
+          console.error(error);
+      } 
+    }
+
+    const onInputChange = e =>{
+      setSearch(e.target.value);
+    }
+
+    useEffect(()=> {
+      getMovie();
+    }, []);
+
+
+  function checkResponse(data){
+     if(data.Response==="True"){
+       return(
+         <div>
+            <img src={data.Poster} alt=""/>
+            <h4>Title: {data.Title}</h4>
+            <p>Year: {data.Year}</p>
+            <p>Writer: {data.Writer}</p>
+            <p>{data.Response}</p>
+         </div>
+       );
+    }
+      return (
+        <p>No Movie found</p>
+      );
+    }
+
+   return(
+      <div>
+         <input type="text" value={search} onChange={onInputChange}/>
+         <button type="submit" onClick={getMovie}>Search</button>
+         <br></br>   
+         {checkResponse(movie)}
+      </div>
+   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
